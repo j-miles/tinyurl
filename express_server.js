d30a1376c997054cf839ce5c5d9db28c67e0408f
Jackie <jaxmiles@gmail.com>
2017-03-16 00:19:17 +0000

    fixed new url generator w/ var random in post

--- express_server.js
+++ express_server.js
@@ -29,20 +29,20 @@ var urlDatabase = {
 
 };
 
-
-app.get("/urls/new", (req, res) => {
-  let templateVars = {
-  username: req.cookies["username"],
-  }
-  res.render("urls_new", templateVars);
-});
-
 app.get("/urls", (req, res) => {
   let templateVars = {
     urls: urlDatabase,
     username: req.cookies["username"]
   };
   res.render("urls_index", templateVars);
+});
+
+
+app.get("/urls/new", (req, res) => {
+  let templateVars = {
+  username: req.cookies["username"],
+  }
+  res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:id", (req, res) => {
@@ -54,27 +54,17 @@ app.get("/urls/:id", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
-app.get("/u/:shortURL", (req, res) => {
-  let longURL = urlDatabase[req.params.shortURL]
-  res.redirect(longURL);
-});
-
 app.post("/urls", (req, res) => {
   console.log(req.body);  // debug statement to see POST parameters
+  var random = generateRandomString();
   urlDatabase[random] = req.body.longURL
-  console.log(urlDatabase)
   var newURL = random;
   res.redirect("urls/" + newURL);
 });
 
-app.post('/urls/login', (req, res) => {
-  res.cookie('username', req.body.username);
-  res.redirect('/urls');
-});
-
-app.post('/urls/logout', (req, res) => {
-  res.clearCookie('username');
-  res.redirect('/urls');
+app.get("/u/:shortURL", (req, res) => {
+  let longURL = urlDatabase[req.params.shortURL]
+  res.redirect(longURL);
 });
 
 app.post("/urls/:id/delete", (req, res) => {
@@ -87,7 +77,15 @@ app.post('/urls/:id', (req, res) => {
   res.redirect('/urls/' + req.params.id);
 });
 
+app.post('/urls/login', (req, res) => {
+  res.cookie('username', req.body.username);
+  res.redirect('/urls');
+});
 
+app.post('/urls/logout', (req, res) => {
+  res.clearCookie('username');
+  res.redirect('/urls');
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
